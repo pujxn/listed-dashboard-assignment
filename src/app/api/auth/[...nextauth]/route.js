bcd540ff518c76a77ae4ca7a6f983445c1e0f815
pujan <pujxnp@gmail.com>
2023-06-04 20:20:02 +0530

    Fixed bugs

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -16,9 +16,10 @@ const handler = NextAuth({
         async signIn() {
             return "/dashboard"
         },
-        // async redirect(url, baseUrl) {
-        //     return baseUrl
-        // }
+        async redirect({ baseUrl }) {
+            console.log(baseUrl)
+            return baseUrl;
+        }
     }
 
 })
